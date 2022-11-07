441ce8106c427cf0bbbb8e680e00d78c8ff376d7
shahbuland <smatiana1@gmail.com>
2022-11-07 16:50:17 -0500

    Finalize base trainer

--- magicarp/trainer/__init__.py
+++ magicarp/trainer/__init__.py
@@ -13,6 +13,9 @@ from magicarp.utils import get_intervals, wandb_start
 class Trainer:
     def __init__(self, model : CrossEncoder, config : TrainConfig):
         self.model : torch.nn.Module = model
+        self.config : TrainConfig = config
+
+        self.model.to(self.config.device)
 
         self.optimizer = optim.AdamW(
             self.model.parameters(),
@@ -34,8 +37,6 @@ class Trainer:
             final_div_factor=final_learning_rate / config.learning_rate,
             anneal_strategy="linear"
         )
-
-        self.config : TrainConfig = config
     
     def load_checkpoint(self, path : str):
         try:
@@ -57,6 +58,7 @@ class Trainer:
         epochs = self.config.num_epochs
         use_wandb = False
         do_val = False
+        do_save = False if self.config.save_dir is None else True
 
         if self.config.wandb_project is not None:
             wandb_start(self.config)
@@ -94,7 +96,7 @@ class Trainer:
                     if use_wandb:
                         wandb.log({"loss": loss})
                 
-                if intervals["save"]:
+                if intervals["save"] and do_save:
                     self.save_checkpoint(self.config.save_dir)
                 
                 if intervals["val"] and do_val:
