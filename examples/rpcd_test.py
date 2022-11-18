6bed7a812fc8bf6c37b07830b4f41fd269f23c26
shahbuland <smatiana1@gmail.com>
2022-11-17 21:50:23 -0500

    Change example to new instruct model

--- examples/rpcd_test.py
+++ examples/rpcd_test.py
@@ -2,18 +2,37 @@ from typing import Iterable
 from torch import Tensor
 import torch
 
-from magicarp.data import TextElement
-from magicarp.pipeline.rpcd import RPCDPipeline
+from magicarp.pipeline.rpcd import InstructRPCD
 
-pipe = RPCDPipeline("data/rpcd", device = "cuda", min_comments=10)
-
-# Load model
-
-from magicarp.models.imgtext import ImgTextEncoder
+from magicarp.models.instruct_imgtext import InstructImgText
 from magicarp.configs import magiCARPConfig
-from magicarp.trainer import Trainer
+from magicarp.trainer.instruct_trainer import InstructTrainer
 
-config = magiCARPConfig.load_yaml("configs/imgtext_config.yml")
-model = ImgTextEncoder(config.model)
-trainer = Trainer(model, config.train)
-trainer.train(pipe)
+if __name__ == "__main__":
+    pipe = InstructRPCD(path="data/rpcd", device = "cuda", min_comments=4, max_comments=9)
+
+    # Load model
+
+    # Loss function from passage re-ranking with BERT
+    # In this case, relevant passages (comments) are those with many upvotes, irrelevant are those
+    # with many downvotes. Dataset is biased towards passages with many upvotes or downvotes
+    # So we assume  from this that passages are strongly relevant or irrelevant
+    def loss_fn(pred_scores, true_scores):
+
+        # mse
+        loss = torch.mean((pred_scores - true_scores)**2)
+        return loss
+
+        #relevant = true_scores > 0.5
+        #pred_scores = torch.sigmoid(pred_scores) # Sigmoid to get probabilities
+        #loss = torch.where(relevant, torch.log(pred_scores), torch.log(1 - pred_scores))
+
+        #return -1 * loss.sum()
+
+    config = magiCARPConfig.load_yaml("configs/imgtext_config.yml")
+
+    model = InstructImgText(config.model)
+
+    trainer = InstructTrainer(model, config.train)
+    trainer.train(pipe)
+
