453184444791272e4981ae11ed7ff93890872841
shahbuland <smatiana1@gmail.com>
2022-11-17 21:54:30 -0500

    remove redundant code

--- examples/rpcd_test.py
+++ examples/rpcd_test.py
@@ -12,25 +12,7 @@ if __name__ == "__main__":
     pipe = InstructRPCD(path="data/rpcd", device = "cuda", min_comments=4, max_comments=9)
 
     # Load model
-
-    # Loss function from passage re-ranking with BERT
-    # In this case, relevant passages (comments) are those with many upvotes, irrelevant are those
-    # with many downvotes. Dataset is biased towards passages with many upvotes or downvotes
-    # So we assume  from this that passages are strongly relevant or irrelevant
-    def loss_fn(pred_scores, true_scores):
-
-        # mse
-        loss = torch.mean((pred_scores - true_scores)**2)
-        return loss
-
-        #relevant = true_scores > 0.5
-        #pred_scores = torch.sigmoid(pred_scores) # Sigmoid to get probabilities
-        #loss = torch.where(relevant, torch.log(pred_scores), torch.log(1 - pred_scores))
-
-        #return -1 * loss.sum()
-
     config = magiCARPConfig.load_yaml("configs/imgtext_config.yml")
-
     model = InstructImgText(config.model)
 
     trainer = InstructTrainer(model, config.train)
